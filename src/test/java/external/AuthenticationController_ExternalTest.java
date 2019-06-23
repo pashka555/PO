34eb7198fa144410e2f6108ae197e03f331e9f31
pashka555 <pprotasenya@gmail.com>
2019-06-24 00:47:05 +0300

    Automated external test for the /load GET request

--- src/test/java/external/AuthenticationController_ExternalTest.java
+++ src/test/java/external/AuthenticationController_ExternalTest.java
@@ -1,6 +1,10 @@
-import jdk.nashorn.internal.objects.Global;
-import jdk.nashorn.internal.parser.JSONParser;
+package external;
+
 import lnu.postoffice.model.Message;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
@@ -8,17 +12,16 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.internal.runners.JUnit38ClassRunner;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 
-import javax.swing.text.html.parser.Entity;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
-public class AuthenticationController_Test {
+@RunWith(MockitoJUnitRunner.class)
+public class AuthenticationController_ExternalTest {
 
     @Test
     public void loadReturnsValidStatusCode() throws IOException {
@@ -32,7 +35,7 @@ public class AuthenticationController_Test {
     }
 
     @Test
-    public void requestingToLoadReturnsMessages() throws IOException {
+    public void requestingToLoadReturnsMessageWithContent() throws IOException, ParseException, NoSuchFieldException {
 
         HttpUriRequest request = new HttpGet( "http://localhost:8080/load" );
 
@@ -40,10 +43,13 @@ public class AuthenticationController_Test {
 
         List<Message> messageList = new LinkedList<Message>();
 
+        JSONParser jsonParser = new JSONParser();
 
+        Object receiver = jsonParser.parse(httpResponse.getEntity().getContent());
 
-        //Assert.assertEquals();
-
-        Assert.assertEquals(HttpStatus.OK.value(), httpResponse.getStatusLine().getStatusCode());
+        for(int i = 0 ; i < ((JSONArray)receiver).size(); i++) {
+            assert (((JSONObject) ((JSONArray) receiver).get(i)).get("nickname") != null);
+            assert (((JSONObject) ((JSONArray) receiver).get(i)).get("content") != null);
+        }
     }
 }
