dcfb116cbe163e6f664657d71efcca550a022a12
pashka555 <pprotasenya@gmail.com>
2019-06-25 00:22:49 +0300

    working on CI

--- src/test/java/external/AuthenticationController_ExternalTest.java
+++ src/test/java/external/AuthenticationController_ExternalTest.java
@@ -1,27 +1,14 @@
 package external;
 
-import lnu.postoffice.model.Message;
-import net.minidev.json.JSONArray;
-import net.minidev.json.JSONObject;
-import net.minidev.json.parser.JSONParser;
-import net.minidev.json.parser.ParseException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.http.HttpStatus;
-
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AuthenticationController_ExternalTest {
 
-
+    @Test
+    public void stubTest() {
+        assert(1==1);
+    }
 }
